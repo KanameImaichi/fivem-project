ebeb35370fcb543c655ed5d6dfe54576931dcdd3
kaname <kaname.imaichi@gmail.com>
2024-04-14 17:35:35 +0900

    debug comment

--- k8s-proxmox/k8s-node-setup.sh
+++ k8s-proxmox/k8s-node-setup.sh
@@ -130,7 +130,8 @@ sysctl --system
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet=1.27.1-00 kubeadm=1.27.1-00 kubectl=1.27.1-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
 # Disable swap
@@ -292,8 +293,6 @@ case $1 in
         exit 1
         ;;
 esac
-
-echo "testyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
 
 # region : setup for first-control-plane node
 
