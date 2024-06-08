027c8994dd7fb875ad810f2c2124a76087d9bd2a
kaname <kaname.imaichi@gmail.com>
2024-06-08 23:39:51 +0900

    create namespace

--- k8s-proxmox/k8s-node-setup.sh
+++ k8s-proxmox/k8s-node-setup.sh
@@ -355,9 +355,15 @@ helm install cilium cilium/cilium \
     --set k8sServicePort=8443
 
 # Install ArgoCD Helm chart
-kubectl create namespace argocd
 helm repo add argo https://argoproj.github.io/argo-helm
-helm install -n argocd argocd argo/argo-cd
+helm install argocd argo/argo-cd \
+    --version 5.36.10 \
+    --create-namespace \
+    --namespace argocd \
+    --values https://raw.githubusercontent.com/KanameImaichi/fivem-project/"${TARGET_BRANCH}"/k8s-manifests/argocd-helm-chart-values.yaml
+helm install argocd argo/argocd-apps \
+    --version 0.0.1 \
+    --values https://raw.githubusercontent.com/KanameImaichi/fivem-project/"${TARGET_BRANCH}"/k8s-manifests/argocd-apps-helm-chart-values.yaml
 
 
 cat <<EOF | kubectl apply -f -
