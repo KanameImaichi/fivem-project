a58c2c2a1f45a67ce648f8ba5854465da85c24c8
kaname <kaname.imaichi@gmail.com>
2024-08-03 23:51:56 +0900

    add iam builder

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -84,7 +84,13 @@ variable "github_actions_roles" {
 variable "github_actions_builder_roles" {
   type = list(string)
   default = [
-    "roles/owner",
+    "roles/run.admin",
+    "roles/storage.admin",
+    "roles/storage.objectAdmin",
+    "roles/iam.serviceAccountUser",
+    "roles/artifactregistry.writer",
+    "roles/secretmanager.admin",
+    "roles/iam.workloadIdentityPoolAdmin"
   ]
 }
 # Roles for service account which will be used to deploy Cloud Run by GHA.
