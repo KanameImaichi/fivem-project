77f71acfab803bb611f1d530db0a5245ad7830f9
kaname <kaname.imaichi@gmail.com>
2024-08-03 20:21:42 +0900

    fix

--- terraform/cloudflare_access_github.tf
+++ terraform/cloudflare_access_github.tf
@@ -1,6 +1,27 @@
 # Terraformで利用するシークレット
-resource "google_secret_manager_secret" "terraform_secrets" {
-  secret_id = "secret"
+import {
+  id = "cloudflare_sso_github_client_id"
+  to = google_secret_manager_secret.cloudflare_sso_github_client_id
+}
+import {
+  id = "cloudflare_sso_github_client_secret"
+  to = google_secret_manager_secret.cloudflare_sso_github_client_secret
+}
+
+resource "google_secret_manager_secret" "cloudflare_sso_github_client_id" {
+  secret_id = "cloudflare_sso_github_client_id"
+  replication {
+    automatic = true
+  }
+}
+
+data "google_secret_manager_secret_version" "gcp_secrets" {
+  secret  = google_secret_manager_secret.cloudflare_sso_github_client_id.id
+  version = "latest"
+}
+
+resource "google_secret_manager_secret" "cloudflare_sso_github_client_secret" {
+  secret_id = "cloudflare_sso_github_client_secret"
   replication {
     automatic = true
   }
@@ -8,14 +29,14 @@ resource "google_secret_manager_secret" "terraform_secrets" {
 
 # シークレットから最新バージョンを取得する設定
 data "google_secret_manager_secret_version" "gcp_secrets" {
-  secret  = google_secret_manager_secret.terraform_secrets.id
+  secret  = google_secret_manager_secret.cloudflare_sso_github_client_secret.id
   version = "latest"
 }
 
 # シークレットの値をローカル変数に格納
 locals {
-  cloudflare_sso_github_client_id     = jsondecode(data.google_secret_manager_secret_version.gcp_secrets.secret_data)["cloudflare_sso_github_client_id"]
-  cloudflare_sso_github_client_secret = jsondecode(data.google_secret_manager_secret_version.gcp_secrets.secret_data)["cloudflare_sso_github_client_secret"]
+  cloudflare_sso_github_client_id = jsondecode(data.google_secret_manager_secret_version.gcp_secrets.secret_data)
+  cloudflare_sso_github_client_secret = jsondecode(data.google_secret_manager_secret_version.gcp_secrets.secret_data)
 }
 
 resource "cloudflare_access_identity_provider" "github_sso" {
