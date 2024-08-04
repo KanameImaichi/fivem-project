a788e71c7590b9c6edf2710224dae556b529a66d
kaname <kaname.imaichi@gmail.com>
2024-08-04 15:29:07 +0900

    fix

--- k8s-proxmox/deploy-k8s-api-cloudflared-resources-to-cp-1.sh
+++ k8s-proxmox/deploy-k8s-api-cloudflared-resources-to-cp-1.sh
@@ -8,7 +8,7 @@ cloudflared_k8s_endpoint_manifest_url="${k8s_definition_base_url}/k8s-manifests/
 # endregion
 
 # region secret リソースの中身を生成する
-cloudflare_cert_pem="$(/bin/bash <(curl -s "${k8s_definition_base_url}/k8s-proxmox/cluster-boot-up/scripts/local-terminal/obtain-cloudflared-cert.sh") "${target_branch}")"
+cloudflare_cert_pem="$(/bin/bash <(curl -s "${k8s_definition_base_url}/k8s-proxmox/obtain-cloudflared-cert.sh") "${target_branch}")"
 
 prerequisite_resources="$(cat <<EOF
 apiVersion: v1
