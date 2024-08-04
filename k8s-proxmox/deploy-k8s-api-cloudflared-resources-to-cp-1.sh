3b2f23ad2fce1f0fbec2c6d296fdca31af759d9c
kaname <kaname.imaichi@gmail.com>
2024-08-04 13:54:05 +0900

    add cloudflared tunnel

---
+++ k8s-proxmox/deploy-k8s-api-cloudflared-resources-to-cp-1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# region manifest URLを生成する
+
+target_branch="$1"
+k8s_definition_base_url="https://raw.githubusercontent.com/KanameImaichi/fivem-project/main"
+cloudflared_k8s_endpoint_manifest_url="${k8s_definition_base_url}/k8s-manifests/apps/cluster-wide-apps/cloudflared-k8s-endpoint/cloudflared-k8s-endpoint.yaml"
+# endregion
+
+# region secret リソースの中身を生成する
+cloudflare_cert_pem="$(/bin/bash <(curl -s "${k8s_definition_base_url}/k8s-proxmox/cluster-boot-up/scripts/local-terminal/obtain-cloudflared-cert.sh") "${target_branch}")"
+
+prerequisite_resources="$(cat <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: cluster-wide-apps
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: cloudflared-tunnel-credential
+  namespace: cluster-wide-apps
+type: Opaque
+data:
+  TUNNEL_CREDENTIAL: "$(echo "${cloudflare_cert_pem}" | base64 -w 0 -)"
+EOF
+)"
+# endregion
+
+# shellcheck disable=SC2029 # ssh command expanded on client side is the expected behaviour
+ssh seichi-onp-k8s-cp-1 "
+cat <<EOF | kubectl apply -f -
+${prerequisite_resources}
+EOF
+"
+
+# shellcheck disable=SC2029 # ssh command expanded on client side is the expected behaviour
+ssh seichi-onp-k8s-cp-1 "kubectl apply -f \"${cloudflared_k8s_endpoint_manifest_url}\""
