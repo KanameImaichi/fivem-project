429d8142884a8d4fce9f63721f1959ce0764b102
kaname <kaname.imaichi@gmail.com>
2024-08-05 10:24:28 +0900

    fix

--- terraform/cloudflare_network_admin_services.tf
+++ terraform/cloudflare_network_admin_services.tf
@@ -4,9 +4,7 @@ resource "cloudflare_certificate_pack" "advanced_cert_for_admin_network" {
   type    = "advanced"
   hosts = [
     local.root_domain,
-    "*.onp-k8s.admin.${local.root_domain}",
     "*.onp.admin.${local.root_domain}",
-    "*.debug.admin.${local.root_domain}",
   ]
   validation_method     = "txt"
   validity_days         = 365
@@ -14,18 +12,18 @@ resource "cloudflare_certificate_pack" "advanced_cert_for_admin_network" {
   cloudflare_branding   = false
 }
 
-resource "cloudflare_access_application" "onp_argocd" {
+resource "cloudflare_access_application" "onp_admin_proxmox" {
   zone_id          = local.cloudflare_zone_id
-  name             = "Argocd administration"
-  domain           = "argocd.onp-k8s.admin${local.root_domain}"
+  name             = "Proxmox administration"
+  domain           = "proxmox.onp.admin.${local.root_domain}"
   type             = "self_hosted"
   session_duration = "24h"
 
   http_only_cookie_attribute = true
 }
 
-resource "cloudflare_access_policy" "onp_argocd" {
-  application_id = cloudflare_access_application.onp_argocd.id
+resource "cloudflare_access_policy" "onp_admin_proxmox" {
+  application_id = cloudflare_access_application.onp_admin_proxmox.id
   zone_id        = local.cloudflare_zone_id
   name           = "Require to be in a GitHub team to access"
   precedence     = "1"
