a1a657df354b3e4ec53f7baf7150746af50dd2c5
kaname <kaname.imaichi@gmail.com>
2024-08-05 10:08:22 +0900

    fix

--- terraform/cloudflare_network_admin_services.tf
+++ terraform/cloudflare_network_admin_services.tf
@@ -14,18 +14,18 @@
 #   cloudflare_branding   = false
 # }
 
-resource "cloudflare_access_application" "onp_admin_proxmox" {
+resource "cloudflare_access_application" "onp_argocd" {
   zone_id          = local.cloudflare_zone_id
-  name             = "Proxmox administration"
-  domain           = "proxmox.onp.admin.${local.root_domain}"
+  name             = "Argocd administration"
+  domain           = "argocd.onp-k8s.admin${local.root_domain}"
   type             = "self_hosted"
   session_duration = "24h"
 
   http_only_cookie_attribute = true
 }
 
-resource "cloudflare_access_policy" "onp_admin_proxmox" {
-  application_id = cloudflare_access_application.onp_admin_proxmox.id
+resource "cloudflare_access_policy" "onp_argocd" {
+  application_id = cloudflare_access_application.onp_argocd.id
   zone_id        = local.cloudflare_zone_id
   name           = "Require to be in a GitHub team to access"
   precedence     = "1"
