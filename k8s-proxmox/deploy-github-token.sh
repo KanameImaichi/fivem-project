f5ae753a5e26a2684f64f7f8a1a3a0835e6de974
kaname <kaname.imaichi@gmail.com>
2024-08-17 13:16:54 +0900

    fix

---
+++ k8s-proxmox/deploy-github-token.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+encoded_token=$(echo "" | base64 -w 0)
+
+
+github_token_secret=$(cat <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: controller-manager
+  namespace: cluster-wide-apps
+type: Opaque
+data:
+  github_token: "${encoded_token}"
+EOF
+)
+# endregion
+
+# shellcheck disable=SC2029 # ssh command expanded on client side is the expected behaviour
+ssh cloudinit@192.168.0.11 "cat <<EOF | kubectl apply -f -
+${github_token_secret}
+EOF
+"
